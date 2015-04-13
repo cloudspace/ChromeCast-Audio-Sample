5905d81ae0df8f04c9d6d7fd3759a56722d6f21e
Isaac Paul <izackp@gmail.com>
2015-04-13 16:01:11 -0400

    Another fix

--- scripts/remove-key.sh
+++ scripts/remove-key.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 security delete-keychain ios-build.keychain
-rm -f "~/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+rm -f "~/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME"
